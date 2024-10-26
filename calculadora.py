d20f37a63bfc6f50f2c394247bcfe3458d86fca7
Luiz-lang <oluizde01@gmail.com>
2024-10-26 13:53:02 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+primeiro = int(input("Digite o  primeiro número: "))
+segundo = int(input("Digite o segundo número: "))
+
+soma  = primeiro + segundo
+subtracao = primeiro - segundo
+multiplicacao = primeiro * segundo
+divisao = primeiro / segundo
+
+print("Soma: ", soma)
+print("Subtração: ", subtracao)
+print("Multiplicação: ", multiplicacao)
+print("Divisão: ", divisao)
